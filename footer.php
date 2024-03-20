9a691bd1979bcd8d5fdc6740e34f03d0a0868629
Pete Hocking <pete.s.hocking@gmail.com>
2024-03-20 13:06:23 +0000

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,19 @@
+</main>
+<!-- Main End -->
+
+<!-- Footer Start -->
+<footer class="footer">
+
+
+<!-- Footer End -->
+</footer>
+
+<!-- WordPress Footer Scripts Start -->
+<section id="wp-footer">
+    <?php wp_footer(); ?>
+</section>
+<!-- WordPress Footer Scripts End -->
+
+</body>
+
+</html>
